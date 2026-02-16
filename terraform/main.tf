335bb13acbe225e8640f1cb2a8681ea162f0eafc
Atul Kamble <atul_kamble@hotmail.com>
2026-02-16 16:39:18 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-# Configure the Azure Provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
